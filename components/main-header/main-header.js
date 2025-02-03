797d59e96fa4782031c714b68e2643745c5fa470
Prince-973 <prinsvaghasiya.it21@scet.ac.in>
2025-02-03 15:00:56 +0530

    add the image slider and populate home page

--- components/main-header/main-header.js
+++ components/main-header/main-header.js
@@ -1,10 +1,12 @@
 import Link from "next/link"
-import logoImg   from '../assets/logo.png'
+import logoImg   from '../../assets/logo.png'
 import classes from  './main-header.module.css'
 import Image from "next/image"
+import MainHeaderBackground from "./main-header-background"
 function Header() {
   return (
     <>
+           <MainHeaderBackground/>
       <header className={classes.header}>
       <Link className={classes.logo} href="/">
         <Image src={logoImg} alt="A plate with food on it" priority />
