ea8c4d829e5ef3751999492143ca44db4c42327c
Prince-973 <prinsvaghasiya.it21@scet.ac.in>
2025-02-03 13:53:36 +0530

    learn about dynamic routing using slug

---
+++ app/blog/[slug]/page.js
@@ -0,0 +1,12 @@
+
+
+function BlogPostPage() {
+  return (
+    <div>
+      <h1>Blog Page</h1>
+
+    </div>
+  )
+}
+
+export default BlogPostPage
--- app/page.js
+++ app/page.js
@@ -1,6 +1,5 @@
 import Link from "next/link";
 import Header from "../components/header";
-
 export default function Home() {
   
   return (
