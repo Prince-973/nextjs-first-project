4e3cc0525e24f9feaf247207a460fb7c82417b6c
Prince <prinsvaghasiya.it21@scet.ac.in>
2025-02-04 01:05:21 +0530

    complete Home Page and [EventId].js dynamic Page

--- next_project/pages/events/[eventId].js
+++ next_project/pages/events/[eventId].js
@@ -1,8 +1,31 @@
+import EventContent from "@/components/event-detail/event-content";
+import EventLogistics from "@/components/event-detail/event-logistics";
+import EventSummary from "@/components/event-detail/event-summary";
+import { getEventById } from "@/dummy-data";
+import { useRouter } from "next/router";
+import { Fragment } from "react";
+
 function Eventdetail() {
+  const router = useRouter();
+
+  const eventId = router.query.eventId;
+  const event = getEventById(eventId);
+  if (!event) {
+    return <p>No Event Found</p>;
+  }
   return (
-    <div>
-      <h1>Event Detail</h1>
-    </div>
+    <Fragment>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.image}
+        imageAlt={event.title}
+        image={event.image}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </Fragment>
   );
 }
 
