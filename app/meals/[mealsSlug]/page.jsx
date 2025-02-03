d3716d01644662878fac1020527801e287ad0d37
Prince-973 <prinsvaghasiya.it21@scet.ac.in>
2025-02-03 14:36:15 +0530

    add header and routes

---
+++ app/meals/[mealsSlug]/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function MealsSlugPage() {
+  return (
+    <div>
+      <h1>Somting</h1>
+    </div>
+  )
+}
+
+export default MealsSlugPage
---
+++ app/meals/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function MealsPage() {
+  return (
+    <div>
+      <h1>Meals</h1>
+    </div>
+  )
+}
+
+export default MealsPage
