516398bcbb74dfb2d5c7b61bc49043eefc8f6589
Prince <prinsvaghasiya.it21@scet.ac.in>
2025-02-05 01:09:56 +0530

    add routes

---
+++ pages/context.js
@@ -0,0 +1,5 @@
+function ContextPage() {
+  return <div></div>;
+}
+
+export default ContextPage;
---
+++ pages/posts/[slug].js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function PostDeatailPage() {
+  return <div></div>;
+}
+
+export default PostDeatailPage;
