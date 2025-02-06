b33e60a3191014ed4ce76442843fe68e66d07ff1
Prince-973 <prinsvaghasiya.it21@scet.ac.in>
2025-02-06 12:47:12 +0530

    complete signup functionality

---
+++ pages/api/auth/signup.js
@@ -0,0 +1,47 @@
+import { hashPassword } from "../../../lib/auth";
+import { connectToDatabase } from "../../../lib/db";
+
+async function handler(req, res) {
+  if (req.method !== "POST") {
+    return res.status(500).json({
+      message: "Invalid request method",
+    });
+  }
+  const data = req.body;
+  const { email, password } = data;
+  if (
+    !email ||
+    !password ||
+    !email.includes("@") ||
+    password.trim().length < 7
+  ) {
+    return res.status(442).json({
+      message:
+        "Invalid email or password (password should be atleast 7 characters long)",
+    });
+  }
+  const client = await connectToDatabase();
+
+  const db = client.db();
+
+  const existingUser = await db.collection("users").findOne({ email });
+  if (existingUser) {
+    client.close();
+    return res.status(442).json({
+      message: "User already exists",
+    });
+  }
+
+  const hashPassword1 = await hashPassword(password);
+  db.collection("users").insertOne({
+    email,
+    password: hashPassword1,
+  });
+
+  res.status(201).json({
+    message: "User created successfully",
+  });
+  client.close();
+}
+
+export default handler;
